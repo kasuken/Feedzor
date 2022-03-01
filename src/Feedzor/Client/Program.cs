1f11585d98589cf509ad53d7c65f1bf9379d3aff
Emanuele Bartolesi <bartolesiemanuele@hotmail.com>
2022-03-01 15:44:34 +0100

    first mudblazor implementation

--- src/Feedzor/Client/Program.cs
+++ src/Feedzor/Client/Program.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using MudBlazor.Services;
 using System;
 using System.Collections.Generic;
 using System.Net.Http;
@@ -21,6 +22,8 @@ namespace Feedzor.Client
 
             builder.Services.AddHttpClient("Feedzor.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
                 .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
+
+            builder.Services.AddMudServices();
 
             builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("Feedzor.ServerAPI"));
 
